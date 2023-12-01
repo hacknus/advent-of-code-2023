96b545381f0a89d5d722abeed9e9ca85b79e310d
hacknus <linus.stoeckli@unibe.ch>
2023-12-01 06:57:38 +0100

    add day two prototype

--- src/days/day_2.rs
+++ src/days/day_2.rs
@@ -1,13 +1,22 @@
+use crate::io::{read_file_lines, read_from_csv};
 use crate::problem::Problem;
 
 pub struct DayTwo {}
 
 impl Problem for DayTwo {
     fn part_one(&self, input: &str) -> String {
+        println!("opening {input}");
+        let contents = read_file_lines(input);
+        println!("found contents {contents:?}");
+
         format!("{}", "Part one not yet implemented.")
     }
 
     fn part_two(&self, input: &str) -> String {
+        println!("opening {input}");
+        let contents = read_from_csv(input, b',');
+        println!("found contents {contents:?}");
+
         format!("{}", "Part two not yet implemented.")
     }
 }
