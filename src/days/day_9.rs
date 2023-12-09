5245f07e599f3df0c4bbe685a7fac58a870bd4c4
hacknus <linus.stoeckli@unibe.ch>
2023-12-09 08:02:47 +0100

    solved puzzle 9

--- src/days/day_9.rs
+++ src/days/day_9.rs
@@ -3,17 +3,86 @@ use crate::problem::Problem;
 
 pub struct DayNine {}
 
+
+pub fn get_diff(vec: &Vec<i32>) -> Vec<i32> {
+    let mut d = vec![];
+    for i in 0..vec.len() - 1 {
+        d.push(vec[i + 1] - vec[i])
+    }
+    d
+}
+
 impl Problem for DayNine {
     fn part_one(&self, input: &str) -> String {
         let contents = read_file_lines(input);
-        dbg!(&contents);
-        format!("{}", "Part one not yet implemented.")
+        let mut values = vec![];
+        for line in contents {
+            values.push(line.split(" ").map(|v| v.parse::<i32>().unwrap()).collect::<Vec<i32>>());
+        }
+
+        let mut sum = 0;
+
+        for history in values.iter_mut() {
+            let mut diffs = vec![history.clone()];
+
+            loop {
+                let diff = get_diff(&diffs.last().unwrap());
+                diffs.push(diff.clone());
+                if diff.iter().filter(|&v| *v == 0 ).count() == diff.len() {
+                    break;
+                }
+            }
+
+            let i = diffs.len()-1;
+
+            diffs[i].push(0);
+
+            for i in (0..diffs.len()-1).rev() {
+                let val = diffs[i+1].last().unwrap() + diffs[i].last().unwrap();
+                diffs[i].push(val);
+            }
+
+            sum += diffs[0].last().unwrap();
+
+        }
+
+        format!("{}", sum)
     }
 
     fn part_two(&self, input: &str) -> String {
         let contents = read_file_lines(input);
-        dbg!(&contents);
-        format!("{}", "Part two not yet implemented.")
+        let mut values = vec![];
+        for line in contents {
+            values.push(line.split(" ").map(|v| v.parse::<i32>().unwrap()).collect::<Vec<i32>>());
+        }
+
+        let mut sum = 0;
+
+        for history in values.iter_mut() {
+            let mut diffs = vec![history.clone()];
+
+            loop {
+                let diff = get_diff(&diffs.last().unwrap());
+                diffs.push(diff.clone());
+                if diff.iter().filter(|&v| *v == 0 ).count() == diff.len() {
+                    break;
+                }
+            }
+
+            let i = diffs.len()-1;
+
+            diffs[i].insert(0,0);
+
+            for i in (0..diffs.len()-1).rev() {
+                let val = - diffs[i+1].first().unwrap() + diffs[i].first().unwrap();
+                diffs[i].insert(0, val);
+            }
+
+            sum += diffs[0].first().unwrap();
+
+        }
+
+        format!("{}", sum)
     }
 }
 
