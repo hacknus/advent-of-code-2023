ddb1830a2dfa5529e6b933cbc1f49d359aeb9955
hacknus <linus.stoeckli@unibe.ch>
2023-12-10 16:27:56 +0100

    day 11 prototype

--- src/days/day_11.rs
+++ src/days/day_11.rs
@@ -1,13 +1,18 @@
+use crate::io::read_file_lines;
 use crate::problem::Problem;
 
 pub struct DayEleven {}
 
 impl Problem for DayEleven {
     fn part_one(&self, input: &str) -> String {
+        let contents = read_file_lines(input);
+        dbg!(&contents);
         format!("{}", "Part one not yet implemented.")
     }
 
     fn part_two(&self, input: &str) -> String {
+        let contents = read_file_lines(input);
+        dbg!(&contents);
         format!("{}", "Part two not yet implemented.")
     }
 }
