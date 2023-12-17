2a521a1fd780534eb8cef299825fbef0bdf9e9d6
hacknus <linus.stoeckli@unibe.ch>
2023-12-17 10:54:24 +0100

    better not try it with recursion...

--- src/days/day_17.rs
+++ src/days/day_17.rs
@@ -1,9 +1,51 @@
+use crate::io::read_file_lines;
 use crate::problem::Problem;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
 
 pub struct DaySeventeen {}
 
+#[derive(Copy, Clone, PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+struct Move {
+    dir: Direction,
+    position: (usize, usize),
+    terminated: bool,
+    heat_loss: u32,
+    consecutive_moves: usize,
+    next_moves: Vec<Rc<RefCell<Move>>>,
+}
+
+impl Move {
+    pub fn propagate(&mut self, map: &Vec<Vec<String>>) {
+        self.heat_loss = map[self.position.1][self.position.0]
+            .parse::<u32>()
+            .unwrap();
+        if self.position == (map[0].len(), map.len()) {
+            // we are at the destination
+            self.terminated = true;
+            return;
+        }
+        match self.dir {
+            Direction::Up => {}
+            Direction::Down => {}
+            Direction::Left => {}
+            Direction::Right => {}
+        }
+    }
+}
+
 impl Problem for DaySeventeen {
     fn part_one(&self, input: &str) -> String {
+        let contents = read_file_lines(input);
+
         format!("{}", "Part one not yet implemented.")
     }
 
