429417bcae9c1d8b6e8e5c2a38d36de6d33d18c0
hacknus <linus.stoeckli@unibe.ch>
2023-12-17 21:43:19 +0100

    dijkstra does not work

--- src/days/day_17.rs
+++ src/days/day_17.rs
@@ -1,10 +1,82 @@
 use crate::io::read_file_lines;
 use crate::problem::Problem;
 use std::cell::RefCell;
-use std::collections::HashMap;
 use std::rc::Rc;
-
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap, HashSet},
+};
 pub struct DaySeventeen {}
+fn dijkstra(
+    start: Vertex,
+    initial_direction: Direction,
+    adjacency_list: &HashMap<Vertex, Vec<(Vertex, i32, Direction)>>,
+) -> HashMap<Vertex, (i32, Direction)> {
+    let mut distances = HashMap::new();
+    let mut visited = HashSet::new();
+    let mut to_visit = BinaryHeap::new();
+
+    distances.insert(start, (0, initial_direction.clone()));
+    to_visit.push(Visit {
+        vertex: start,
+        directions: vec![initial_direction.clone()],
+        distance: 0,
+    });
+
+    while let Some(Visit {
+        vertex, distance, ..
+    }) = to_visit.pop()
+    {
+        if !visited.insert(vertex) {
+            // Already visited this node
+            continue;
+        }
+
+        if let Some(neighbors) = adjacency_list.get(&vertex) {
+            for (neighbor, cost, direction) in neighbors {
+                let new_distance = distance as i32 + cost;
+                let is_shorter = distances
+                    .get(&neighbor)
+                    .map_or(true, |&(current, _)| new_distance < current);
+
+                let mut directions = if let Some(v) = to_visit.iter().last() {
+                    v.directions.clone()
+                } else {
+                    vec![]
+                };
+                directions.push(direction.clone());
+                let n = directions.len();
+                let three_consecutive_moves = if n > 3 {
+                    directions[n - 1] == directions[n - 2]
+                        && directions[n - 1] == directions[n - 3]
+                        && directions[n - 1] == directions[n - 4]
+                } else {
+                    false
+                };
+
+                if is_shorter {
+                    if three_consecutive_moves {
+                        distances.insert(*neighbor, (new_distance, direction.clone()));
+                        to_visit.push(Visit {
+                            vertex: *neighbor,
+                            directions,
+                            distance: usize::MAX,
+                        });
+                    } else {
+                        distances.insert(*neighbor, (new_distance, direction.clone()));
+                        to_visit.push(Visit {
+                            vertex: *neighbor,
+                            directions,
+                            distance: new_distance as usize,
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    distances
+}
 
 #[derive(Clone, Debug, PartialEq)]
 enum Direction {
@@ -34,187 +106,127 @@ impl Direction {
     }
 }
 
-#[derive(Clone, Debug)]
-struct Node {
-    parent: Option<Rc<RefCell<Node>>>,
-    position: (isize, isize),
-    directions: Vec<Direction>,
-    g: i32,
-    h: i32,
-    f: i32,
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct Vertex {
+    x: usize,
+    y: usize,
+    heat_loss: u32,
 }
 
-fn a_star(
-    map: &Vec<Vec<i32>>,
-    initial_direction: Direction,
-    start: (isize, isize),
-    end: (isize, isize),
-) -> Option<Vec<((isize, isize), Direction)>> {
-    // create start and end node
-    let start_node = Node {
-        parent: None,
-        position: start,
-        directions: vec![initial_direction],
-        g: 0,
-        h: 0,
-        f: 0,
-    };
-    let end_node = Node {
-        parent: None,
-        position: end,
-        directions: vec![],
-        g: 0,
-        h: 0,
-        f: 0,
-    };
-
-    // initialize both open and closed list
-    let mut open_list = vec![];
-    let mut closed_list = vec![];
-
-    open_list.push(start_node);
-
-    // loop until we find the end
-    while open_list.len() > 0 {
-        let mut current_node = open_list[0].clone();
-        let mut current_index = 0;
-
-        for (index, item) in open_list.iter().enumerate() {
-            if item.f < current_node.f {
-                current_node = item.clone();
-                current_index = index
-            }
-        }
-
-        // Pop current off open list, add to closed list
-        open_list.remove(current_index);
-        closed_list.push(current_node.clone());
-
-        // Found the end
-        if current_node.position == end_node.position {
-            let mut path = vec![(
-                current_node.position,
-                current_node.directions.last().unwrap().clone(),
-            )];
-            let mut current = current_node.clone();
-            while let Some(parent) = current.parent {
-                path.push((
-                    parent.borrow().position,
-                    parent.borrow().directions.last().unwrap().clone(),
-                ));
-                current = parent.borrow().clone();
-            }
-            return Some(path);
-        }
-
-        // generate children
-
-        let mut children = vec![];
-        for new_direction in [
-            Direction::Up,
-            Direction::Down,
-            Direction::Left,
-            Direction::Right,
-        ] {
-            // Get node position
-            let node_position = (
-                current_node.position.0 + new_direction.as_coordinates().0,
-                current_node.position.1 + new_direction.as_coordinates().1,
-            );
-
-            // Make sure within range
-            if node_position.0 >= map[0].len() as isize
-                || node_position.0 < 0
-                || node_position.1 >= map.len() as isize
-                || node_position.1 < 0
-            {
-                continue;
-            }
-
-            // Make sure walkable terrain
-            // if map[node_position.1 as usize][node_position.0 as usize] != 0 {
-            //     continue;
-            // }
-
-            let mut directions = current_node.directions.clone();
-            directions.push(new_direction);
-
-            // Create new node
-            let new_node = Node {
-                parent: Some(Rc::new(RefCell::new(current_node.clone()))),
-                position: node_position,
-                directions,
-                g: 0,
-                h: 0,
-                f: 0,
-            };
-
-            // Append
-            children.push(new_node);
-        }
-        // Loop through children
-        'outer: for mut child in children.iter_mut() {
-            // Child is on the closed list
-            for closed_child in closed_list.iter() {
-                if child.position == closed_child.position {
-                    continue 'outer;
-                }
-            }
-
-            // logic for 3 moves in a row:
-            let n = child.directions.len();
-            if n >= 4 {
-                if child.directions[n - 1] == child.directions[n - 2]
-                    && child.directions[n - 1] == child.directions[n - 3]
-                    && child.directions[n - 1] == child.directions[n - 4]
-                {
-                    continue;
-                }
-            }
-
-            // Create the f, g, and h values
-            child.g =
-                current_node.g + 1 + map[child.position.1 as usize][child.position.0 as usize];
-            // child.g = current_node.g + 1;
-            child.h = ((child.position.0 as i32 - end_node.position.0 as i32).pow(2))
-                + ((child.position.1 as i32 - end_node.position.1 as i32).pow(2));
-            child.h = 0;
-            child.f = child.g + child.h;
-
-            // Child is already in the open list
-            for open_node in open_list.iter() {
-                if child.position == open_node.position && child.g > open_node.g {
-                    continue 'outer;
-                }
-            }
-
-            // Add the child to the open list
-            open_list.push(child.clone());
-        }
+impl Vertex {
+    fn new(x: usize, y: usize, heat_loss: u32) -> Vertex {
+        Vertex { x, y, heat_loss }
     }
-    None
 }
+
+#[derive(Debug)]
+struct Visit<V> {
+    vertex: V,
+    directions: Vec<Direction>,
+    distance: usize,
+}
+
+impl<V> Ord for Visit<V> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.distance.cmp(&self.distance)
+    }
+}
+
+impl<V> PartialOrd for Visit<V> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<V> PartialEq for Visit<V> {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance.eq(&other.distance)
+    }
+}
+
+impl<V> Eq for Visit<V> {}
 
 impl Problem for DaySeventeen {
     fn part_one(&self, input: &str) -> String {
         let contents = read_file_lines(input);
         let mut map = vec![vec![0; contents[0].len()]; contents.len()];
+        let mut vertices = HashMap::new();
         for (y, line) in contents.iter().enumerate() {
             for (x, c) in line.char_indices() {
                 map[y][x] = c.to_digit(10).unwrap() as i32;
+                vertices.insert(
+                    (x as isize, y as isize),
+                    Vertex::new(x, y, c.to_digit(10).unwrap()),
+                );
             }
         }
 
-        let start = (1, 0);
-        let end = (map[0].len() as isize - 1, map.len() as isize - 1);
-        let initial_direction = Direction::Right;
+        let mut adjacency_list = HashMap::new();
+        for position in vertices.keys() {
+            let mut neighbours = vec![];
 
-        let path = a_star(&map, initial_direction, start, end).unwrap();
+            let direction = Direction::Left.as_coordinates();
+            let mut neighbour_position = position.clone();
+            neighbour_position.0 += direction.0;
+            if neighbour_position.0 >= 0 {
+                neighbours.push((
+                    vertices[&neighbour_position],
+                    map[neighbour_position.1 as usize][neighbour_position.0 as usize],
+                    Direction::Left,
+                ))
+            }
+
+            let direction = Direction::Right.as_coordinates();
+            let mut neighbour_position = position.clone();
+            neighbour_position.0 += direction.0;
+            if neighbour_position.0 < map[0].len() as isize {
+                neighbours.push((
+                    vertices[&neighbour_position],
+                    map[neighbour_position.1 as usize][neighbour_position.0 as usize],
+                    Direction::Right,
+                ))
+            }
+
+            let direction = Direction::Up.as_coordinates();
+            let mut neighbour_position = position.clone();
+            neighbour_position.1 += direction.1;
+            if neighbour_position.1 >= 0 {
+                neighbours.push((
+                    vertices[&neighbour_position],
+                    map[neighbour_position.1 as usize][neighbour_position.0 as usize],
+                    Direction::Up,
+                ))
+            }
+
+            let direction = Direction::Down.as_coordinates();
+            let mut neighbour_position = position.clone();
+            neighbour_position.1 += direction.1;
+            if neighbour_position.1 < map.len() as isize {
+                neighbours.push((
+                    vertices[&neighbour_position],
+                    map[neighbour_position.1 as usize][neighbour_position.0 as usize],
+                    Direction::Down,
+                ))
+            }
+
+            adjacency_list.insert(vertices[position], neighbours);
+        }
+
+        let distances = dijkstra(vertices[&(1, 0)], Direction::Right, &adjacency_list);
+
+        let mut heat_loss = 0;
+        for (v, (d, _)) in &distances {
+            if v.x == map[0].len() - 1 && v.y == map.len() - 1 {
+                println!("name: ({},{}), distance: {}", v.x, v.y, d);
+                heat_loss = *d;
+            }
+        }
+
         for (y, line) in map.iter().enumerate() {
             for (x, local_heat_loss) in line.iter().enumerate() {
                 let mut b = false;
-                for ((x_p, y_p), dir) in path.iter() {
-                    if x == *x_p as usize && y == *y_p as usize {
+                for (v, (d, dir)) in distances.iter() {
+                    if x == v.x && y == v.y {
                         b = true;
                         print!("{}", dir.to_string());
                     }
@@ -225,10 +237,7 @@ impl Problem for DaySeventeen {
             }
             println!();
         }
-        let heat_loss = path
-            .iter()
-            .map(|((x, y), _)| map[*y as usize][*x as usize])
-            .sum::<i32>();
+
         format!("{}", heat_loss)
     }
 
